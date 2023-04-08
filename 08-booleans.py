47fc8d8db39dfa63432f1ab7e9b95b369b92d11a
Nicolas Cutler <65867770+000Nobody@users.noreply.github.com>
2023-04-08 07:13:16 -0400

    Update 08-booleans.py

---
+++ 08-booleans.py
@@ -0,0 +1,13 @@
+# A boolean can only be one of two things: True or False
+# When you compare two variables, for example, it will return a boolean
+
+print(6 == 3+3)
+# This will return True
+
+# You can store Boolean values in variables, just like with integers or strings
+
+my_bool = True
+if my_bool:
+  print("Boolean is true!")
+  
+ 
