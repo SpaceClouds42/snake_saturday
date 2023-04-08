ca7f36e1bfd3179799f0d0809072b7f7d7423a10
SpaceClouds42 <spaceclouds42@gmail.com>
2023-04-07 21:49:41 -0400

    init

---
+++ 2-comments.py
@@ -0,0 +1,8 @@
+# <-- this hash symbol creates a 'comment'
+# anything after a hash will be ignored
+
+# so line 6 will not run
+print("you can see me")
+# print("now you can't")
+
+# we use comments to describe our code
---
+++ 3-variables.py
@@ -0,0 +1,8 @@
+my_variable = "Hello, world!" # here, we create a 'variable', a device that stores data
+print(my_variable)
+
+my_number_variable = 0 # variables can store all kinds of data, like numbers
+my_list_variable = [0, 1] # and lists
+
+print(my_number_variable)
+print(my_list_variable)
